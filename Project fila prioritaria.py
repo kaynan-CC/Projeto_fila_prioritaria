1437274ba634be2aae320f87664899d792c66e91
kaynan-CC <kaynanestudos981@gmail.com>
2026-08-22 21:18:33 -0300

    style: Adicionado as cores do projeto

---
+++ Project fila prioritaria.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+# Cores
+verde = '\033[1;32m'
+vermelho = '\033[1;31m'
+vermelho_suave = '\033[38;5;203m'
+amarelo = '\033[1;33m'
+ciano = '\033[1;36m'
+magenta = '\033[1;35m'
+fim = '\033[0m'
