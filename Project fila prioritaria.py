3c13ba71da56174192e3f8e9ef5b2dfcf5ace46d
kaynan-CC <kaynanestudos981@gmail.com>
2026-08-22 21:34:04 -0300

    feat: Adicionado as opções de subtriagem e consciencia do paciente.

--- Project fila prioritaria.py
+++ Project fila prioritaria.py
@@ -45,3 +45,34 @@ while True:
             print(f"{vermelho}Idade inválida! Digite apenas números inteiros (Ex: 25).{fim}")
             
         sleep(2)
+                
+        while True:
+            print(f"{magenta}----PACIENTE ESTÁ CONSCIENTE?----{fim}")
+            print("1 - Sim")
+            print("2 - Não")
+            escolha0 = input("Digite um número: ").strip()
+            if escolha0 == "1" or escolha0 == "2":
+                break
+            print(f"{vermelho}Opção inválida! Digite 1 para Sim ou 2 para Não.{fim}\n")
+            
+        sleep(1)
+        
+        if escolha0 == "2":
+            print(f"{vermelho}Paciente: {Nome} | Idade: {Idade}\nPrioridade CRÍTICA \nAtendimento Imediato.{fim}")
+            sleep(3)
+            continue
+            
+        elif escolha0 == "1":
+            sleep(2)
+            
+        while True:
+            print(f"{magenta}--------------------Subtriagem--------------------{fim}")
+            print("1 - Respiratória")
+            print("2 - Dor")
+            print("3 - Neurológica")
+            print("4 - Preferencial")
+            sintoma_principal = input("Selecione a categoria principal: ").strip()
+            if sintoma_principal in ["1", "2", "3", "4"]:
+                break
+            print(f"{vermelho}Opção inválida! Escolha de 1 a 4.{fim}\n")
+            
