7794f0d4286364f33a08ef2cd1f362826c2655b4
kaynan-CC <kaynanestudos981@gmail.com>
2026-08-22 21:37:10 -0300

    feat: Adicionado as opções de subtriagem Dor e Respiratória

--- Project fila prioritaria.py
+++ Project fila prioritaria.py
@@ -75,4 +75,51 @@ while True:
             if sintoma_principal in ["1", "2", "3", "4"]:
                 break
             print(f"{vermelho}Opção inválida! Escolha de 1 a 4.{fim}\n")
-            
+                        
+        # ---------------- SUBTRIAGEM RESPIRATÓRIA ----------------
+        if sintoma_principal == "1":
+            sleep(2)
+            while True:
+                print(f"{magenta}----SELECIONE O ESTADO DO PACIENTE----{fim}")
+                print('1 - Não consegue falar')
+                print('2 - Respiração muito difícil')
+                print('3 - Falta de ar moderada')
+                print('4 - Desconforto leve')
+                escolha = input('Digite o número do sintoma: ').strip()
+                if escolha in ["1", "2", "3", "4"]:
+                    break
+                print(f"{vermelho}Opção inválida! Escolha de 1 a 4.{fim}\n")
+                    
+            sleep(3)
+            if escolha == "1":
+                print(f"{vermelho}Paciente: {Nome} | Idade: {Idade}\nPrioridade CRÍTICA \nAtendimento Imediato.{fim}")
+            elif escolha == "2":
+                print(f"{vermelho_suave}Paciente: {Nome} | Idade: {Idade}\nPrioridade MUITO ALTA \nAtendimento em até 10 minutos.{fim}")
+            elif escolha == "3":
+                print(f"{amarelo}Paciente: {Nome} | Idade: {Idade}\nPrioridade ALTA \nAtendimento em até 30 minutos.{fim}")
+            elif escolha == "4":
+                print(f"{verde}Paciente: {Nome} | Idade: {Idade}\nPrioridade BAIXA \nAtendimento em até 120 minutos.{fim}")
+
+        # ---------------- SUBTRIAGEM DOR ----------------
+        elif sintoma_principal == "2":
+            sleep(3)
+            while True:
+                print(f"{magenta}----SELECIONE O NÍVEL DE DOR----{fim}")
+                print("1 - Dor Intensa")
+                print("2 - Dor forte persistente")
+                print("3 - Dor moderada")
+                print("4 - Dor leve")
+                escolha = input("Digite o número do sintoma: ").strip()
+                if escolha in ["1", "2", "3", "4"]:
+                    break
+                print(f"{vermelho}Opção inválida! Escolha de 1 a 4.{fim}\n")
+                    
+            sleep(1)
+            if escolha == "1":
+                print(f"{vermelho}Paciente: {Nome} | Idade: {Idade}\nPrioridade CRÍTICA \nAtendimento Imediato.{fim}")
+            elif escolha == "2":
+                print(f"{vermelho_suave}Paciente: {Nome} | Idade: {Idade}\nPrioridade MUITO ALTA \nAtendimento em até 10 minutos.{fim}")
+            elif escolha == "3":
+                print(f"{amarelo}Paciente: {Nome} | Idade: {Idade}\nPrioridade ALTA \nAtendimento em até 30 minutos.{fim}")
+            elif escolha == "4":
+                print(f"{verde}Paciente: {Nome} | Idade: {Idade}\nPrioridade BAIXA \nAtendimento em até 120 minutos.{fim}")
